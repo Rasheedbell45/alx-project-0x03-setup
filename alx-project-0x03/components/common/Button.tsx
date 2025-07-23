d40a8f5189359add49394c510b409ffc3f2410cc
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 07:15:27 +0100

    Update Button.tsx

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -5,34 +5,23 @@ interface ButtonProps {
   action?: () => void
 }
 
-
-const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
-
-  const backgroundColorClass = buttonBackgroundColor ? {
-    red: 'bg-red-500',
-    blue: 'bg-blue-500',
-    orange: 'bg-orange-500',
-    green: 'bg-green-500',
-  }[buttonBackgroundColor] : 'bg-slate-500'
-
-
-  return (
-    <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}>
-      {buttonLabel}
-    </button>
-  )
-}
-
-export default Button;
-
 import React from 'react';
 import { ButtonProps } from '@/interface';
 
-const Button: React.FC<ButtonProps> = ({ action, buttonLabel, buttonBackgroundColor }) => {
+const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
+  const backgroundColorClass = buttonBackgroundColor
+    ? {
+        red: 'bg-red-500',
+        blue: 'bg-blue-500',
+        orange: 'bg-orange-500',
+        green: 'bg-green-500',
+      }[buttonBackgroundColor]
+    : 'bg-slate-500';
+
   return (
     <button
       onClick={action}
-      className={`px-4 py-2 text-white rounded-md bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600`}
+      className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}
     >
       {buttonLabel}
     </button>
